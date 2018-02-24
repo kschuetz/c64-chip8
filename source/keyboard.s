340814b8aa57717eddb9741c3842e6040d137410
Kevin Schuetz <schuetzk@gmail.com>
2018-02-24 00:42:03 -0600

    Cleanup

--- source/keyboard.s
+++ source/keyboard.s
@@ -1,9 +1,19 @@
-.export check_keyboard, is_guest_key_pressed, get_guest_keypress, init_keyboard, check_ui_keys
-.export chip8_key_port_a, chip8_key_port_b
-.exportzp ui_key_events, kbd_col0, key_repeat_mode
+.include "common.s"
+
+.export check_keyboard
+.export check_ui_keys
+.export chip8_key_port_a
+.export chip8_key_port_b
+.export get_guest_keypress
+.export init_keyboard
+.export is_guest_key_pressed
+.exportzp kbd_col0
+.exportzp key_repeat_mode
+.exportzp ui_key_events
 
 .import debug_output_hex
-.importzp active_keymap, irq_zp0
+.importzp active_keymap
+.importzp irq_zp0
 
 .zeropage
 kbd_col0:		    .res 16
--- source/memory.s
+++ source/memory.s
@@ -1,10 +1,41 @@
 .include "common.s"
 
+.export buttons_sprite_set
+.export chrome_charset
+.export chrome_color_origin
+.export chrome_origin
 .export clear_ram
+.export guest_ram
+.export guest_screen_color_origin
+.export guest_screen_origin
+.export host_screen
+.export program_start
+.export screen_charset
+.export sprite_pointers
+.export stack_high
+.export stack_low
+.exportzp guest_ram_page
 
 .import fill
-.import guest_ram
 .importzp zp0
+
+guest_ram = $c000
+guest_ram_page = >guest_ram
+program_start = guest_ram + $0200
+stack_low = $9f00		    ; low bytes of return addresses
+stack_high = $9e00	        ; high bytes of return addresses
+host_screen = $b000
+screen_charset = $b800	
+chrome_charset = $a800
+buttons_sprite_set = $a000
+sprite_pointers = host_screen + 1016
+
+guest_screen_origin = host_screen + 40 * guest_screen_offset_y + guest_screen_offset_x
+
+guest_screen_color_origin = COLOR_RAM + 40 * guest_screen_offset_y + guest_screen_offset_x
+
+chrome_origin = host_screen + 40 * (guest_screen_offset_y + guest_screen_physical_height + 1)
+chrome_color_origin = COLOR_RAM + 40 * (guest_screen_offset_y + guest_screen_physical_height + 1)
 
 .proc clear_ram
 			istore zp0, guest_ram
